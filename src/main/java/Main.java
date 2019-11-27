74fa34f5f28ef41da6c7c821adc2a7a2e0ad9145
adam robinson <adam.james.robinson@hotmail.co.uk>
2019-11-27 16:11:58 +0000

    improved the show look

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
 
 
-    public static Player player = new Player("Adam", 100,15,20,"true", 0, 0);
+    public static Player player = new Player("Adam", 100,15,20,"true", 100, 0);
     public static Player enemy = new Player("Ork", 80,10,10,"true", 0 , 0 );
 
     public static Game game = new Game(player, enemy);
