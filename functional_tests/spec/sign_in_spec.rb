7c39c5c230b8f8fbc8bf49c7b3369c5a925c2f00
vikash <zero_transfer@outlook.com>
2019-11-27 15:44:00 +0000

    done all capybara tests i can do at this time

--- functional_tests/spec/sign_in_spec.rb
+++
@@ -1,7 +0,0 @@
-require "spec_helper"
-feature "sign in" do
-  scenario "I should be able to visit the sign in page" do
-    visit '/sign_in'
-    expect(page).to have_content 'Sign In to Battle!'
-  end
-end
