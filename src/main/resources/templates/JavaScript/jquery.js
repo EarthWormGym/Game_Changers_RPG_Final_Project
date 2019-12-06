fdb98d82d5704283bab54a0598d6e646118ac156
Luke Gittins <55198283+lukegittins01@users.noreply.github.com>
2019-12-06 12:48:51 +0000

    Demoday changes

--- src/main/resources/templates/JavaScript/jquery.js
+++ src/main/resources/templates/JavaScript/jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#playerAttack').click(function() {
-        $.get('https://secret-harbor-57457.herokuapp.com//battleJson', function(data) {
+        $.get('http://localhost:4567/battleJson', function(data) {
 //                $('#player_hp').text(data.playersArray[0]["health"]);
                 $('#enemy_hp').text(data.playersArray[1]["health"]);
         });
